18f6fd958e5a3e66b94f26f74f1f85f19d661590
SHY-KG <tjdghks3078etc@gmail.com>
2021-12-01 22:14:26 +0900

    nestjs-versioning-and-swagger #34

--- backend/src/wau/entities/locationinfo.entity.ts
+++ backend/src/wau/entities/locationinfo.entity.ts
@@ -1,11 +1,18 @@
-import { CoreEntity } from 'src/common/entities/core.entity';
 import { Entity, Column } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { CoreEntity } from 'src/common/entities/core.entity';
 
 @Entity()
 export class LocationInfo extends CoreEntity {
+  @ApiProperty({
+    default: 35.73805386139952,
+  })
   @Column('decimal', { precision: 9, scale: 6 })
   lat: number;
 
+  @ApiProperty({
+    default: 139.6538817110336,
+  })
   @Column('decimal', { precision: 9, scale: 6 })
   lng: number;
 }
--- backend/src/wau/wau.controller.ts
+++ backend/src/wau/wau.controller.ts
@@ -1,23 +1,41 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Version } from '@nestjs/common';
 import { WauService } from './wau.service';
 import { Posting } from './entities/posting.entity';
 import { LocationInfo } from './entities/locationinfo.entity';
 import { CreatePostingDto } from './dtos/create-posting.dto';
+import {
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
-@Controller('v1/wau')
+@ApiTags('wau')
+@Controller('')
 export class WauController {
   constructor(private readonly wauService: WauService) {}
 
+  @ApiOkResponse({ type: Posting })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  @Version('1')
   @Get('posting/:id')
   getPostingById(@Param('id') id: string): Promise<Posting[]> {
     return this.wauService.getPostingById(id);
   }
 
+  @ApiOkResponse({ type: [LocationInfo] })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  @Version('1')
   @Get('locations')
   async getLocationinfo(): Promise<LocationInfo[]> {
     return this.wauService.getLocationInfo();
   }
 
+  @ApiCreatedResponse({
+    description: 'The record has been successfully created.',
+  })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  @Version('1')
   @Post('posting')
   createPosting(@Body() createPostingDto: CreatePostingDto) {
     return this.wauService.createPosting(createPostingDto);
