b56394a019d85fd2815b438988128c9aa6752aa8
SHY-KG <tjdghks3078etc@gmail.com>
2021-12-03 23:08:31 +0900

    Backend-Upload-Contents #26

--- backend/src/wau/wau.controller.ts
+++ backend/src/wau/wau.controller.ts
@@ -1,14 +1,27 @@
-import { Body, Controller, Get, Param, Post, Version } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+  Version,
+} from '@nestjs/common';
 import { WauService } from './wau.service';
 import { Posting } from './entities/posting.entity';
 import { LocationInfo } from './entities/locationinfo.entity';
 import { CreatePostingDto } from './dtos/create-posting.dto';
 import {
+  ApiBody,
+  ApiConsumes,
   ApiCreatedResponse,
   ApiForbiddenResponse,
   ApiOkResponse,
   ApiTags,
 } from '@nestjs/swagger';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 
 @ApiTags('wau')
 @Controller('')
@@ -40,4 +53,80 @@ export class WauController {
   createPosting(@Body() createPostingDto: CreatePostingDto) {
     return this.wauService.createPosting(createPostingDto);
   }
+
+  // see.https://docs.nestjs.com/openapi/operations#file-upload
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'file',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', { dest: './contents' }))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    // [TODO] Insert Conetents DB Filename
+    // Mock
+    // {
+    //   fieldname: 'file',
+    //   originalname: 'S__83058692.png',
+    //   encoding: '7bit',
+    //   mimetype: 'image/png',
+    //   destination: './contents',
+    //   filename: 'ce62dae4834ba35dbc2abfa348ed6fa7',
+    //   path: 'contents/ce62dae4834ba35dbc2abfa348ed6fa7',
+    //   size: 490294
+    // }
+  }
+
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        files: {
+          type: 'array',
+          items: {
+            type: 'file',
+            format: 'binary',
+          },
+        },
+      },
+    },
+  })
+  @Post('uploads')
+  @UseInterceptors(FilesInterceptor('files', 5, { dest: './contents' }))
+  uploadFiles(@UploadedFiles() files: Array<Express.Multer.File>) {
+    console.log(files);
+  }
+  // [TODO] Insert Conetents DB Filename
+  // Mock
+  // [
+  //   {
+  //     fieldname: 'files',
+  //     originalname: 'kittykitty.jpeg',
+  //     encoding: '7bit',
+  //     mimetype: 'image/jpeg',
+  //     destination: './contents',
+  //     filename: '2b5983ca6eabe22176cf07cd1c054681',
+  //     path: 'contents/2b5983ca6eabe22176cf07cd1c054681',
+  //     size: 3008271
+  //   },
+  //   {
+  //     fieldname: 'files',
+  //     originalname: 'S__83058692.png',
+  //     encoding: '7bit',
+  //     mimetype: 'image/png',
+  //     destination: './contents',
+  //     filename: 'e4dc6e358d2a460238383b2c9c23c17c',
+  //     path: 'contents/e4dc6e358d2a460238383b2c9c23c17c',
+  //     size: 490294
+  //   }
+  // ]
 }
