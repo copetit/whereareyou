737fb03599a02083243761abf123b7ee611aadaf
SHY-KG <tjdghks3078etc@gmail.com>
2022-01-15 16:25:29 +0900

    add-swagger-update-dtoid #244

--- backend/src/wau/dtos/update-posting.dto.ts
+++ backend/src/wau/dtos/update-posting.dto.ts
@@ -2,6 +2,9 @@ import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreatePostingDto, CreatePostingOutput } from './create-posting.dto';
 
 export class UpdatePostingDto extends PartialType(CreatePostingDto) {
+  @ApiProperty({
+    default: 1,
+  })
   id: number;
 }
 
