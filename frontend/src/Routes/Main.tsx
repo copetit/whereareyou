12e03c04b95d4be96f82df6f265848a3b500aeee
han <copetit412@gmail.com>
2022-01-29 17:18:28 +0900

    仮のボタン作成 #76

--- frontend/src/Routes/Main.tsx
+++ frontend/src/Routes/Main.tsx
@@ -1,11 +1,13 @@
 import Header from './Header';
 import Map from './Map';
+import PostingButton from './PostingButton';
 
 function Main() {
   return (
     <div id="container">
       <Header />
       <Map />
+      <PostingButton />
     </div>
   );
 }
