acaceb56b6ba3933ad4e0feba2a56049f0c67d42
han <copetit412@gmail.com>
2022-02-01 21:49:59 +0900

    refactoring #74

--- frontend/src/Routes/Map.tsx
+++ frontend/src/Routes/Map.tsx
@@ -51,90 +51,86 @@ function Map() {
   }, []);
 
   return (
-    <>
-      <div id="map" className="h-92/100 w-full">
-        <LoadScript
-          googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY || 'dummy'}
+    <div id="map" className="h-92/100 w-full">
+      <LoadScript
+        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY || 'dummy'}
+      >
+        {/* map表示の初期値 */}
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={location}
+          zoom={17}
         >
-          {/* map表示の初期値 */}
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={location}
-            zoom={17}
-          >
-            {results.map((result: IGetLocations) => {
-              return (
-                <>
-                  <Marker
-                    key={result.id}
-                    position={{
-                      lat: Number(result.lat),
-                      lng: Number(result.lng),
-                    }}
-                    onClick={() => {
-                      setSelected(result.id);
-                      getPosting(result.id).then((res) => {
-                        setPostingInfo(res);
-                      });
-                    }}
-                  >
-                    {/* MarkerをクリックするとinfoWindowが表示される */}
-                    {selected === result.id && postingInfo && (
-                      <InfoWindow
-                        onCloseClick={() => {
-                          setdisplayFlg(false);
-                          setSelected(null);
-                        }}
-                      >
-                        <>
-                          {/* {result.id}の情報 */}
-                          <div className="mini-profile min-h-profileCard min-w-profileCard flex max-w-3xl max-h-96">
-                            <div className="img">
-                              <img
-                                className="object-cover w-full h-full relative -left-8 rounded-r-profileCard"
-                                src={`${process.env.REACT_APP_API_URL}/${postingInfo.contents.imageUrl[0]}`}
-                                alt="pet"
-                              />
-                            </div>
-                            <div className="profile-text py-4 pr-4 relatvie">
-                              <p className="font-semibold text-3xl mb-4 overflow-hidden whitespace-nowrap overflow-ellipsis">
-                                {postingInfo.PetName}
-                              </p>
-                              <p className="h-3/5 overflow-scroll">
-                                {postingInfo.PetInfo}
-                              </p>
-                              <button
-                                className="detailPageBtn w-12 top-1/4 -right-12 bg-yellow-400 absolute h-1/2"
-                                onClick={() => {
-                                  setdisplayFlg(true);
-                                }}
-                              >
-                                <Arrow />
-                              </button>
-                            </div>
+          {results.map((result: IGetLocations) => {
+            return (
+              <>
+                <Marker
+                  key={result.id}
+                  position={{
+                    lat: Number(result.lat),
+                    lng: Number(result.lng),
+                  }}
+                  onClick={() => {
+                    setSelected(result.id);
+                    getPosting(result.id).then((res) => {
+                      setPostingInfo(res);
+                    });
+                  }}
+                >
+                  {/* MarkerをクリックするとinfoWindowが表示される */}
+                  {selected === result.id && postingInfo && (
+                    <InfoWindow
+                      onCloseClick={() => {
+                        setdisplayFlg(false);
+                        setSelected(null);
+                      }}
+                    >
+                      <>
+                        {/* {result.id}の情報 */}
+                        <div className="mini-profile min-h-profileCard min-w-profileCard flex max-w-3xl max-h-96">
+                          <div className="img">
+                            <img
+                              className="object-cover w-full h-full relative -left-8 rounded-r-profileCard"
+                              src={`${process.env.REACT_APP_API_URL}/${postingInfo.contents.imageUrl[0]}`}
+                              alt="pet"
+                            />
                           </div>
-                          <p className="absolute bottom-3 right-3 text-gray-500 ">
-                            {new Date(
-                              postingInfo.LostDate,
-                            ).toLocaleDateString()}
-                          </p>
-                        </>
-                      </InfoWindow>
-                    )}
-                  </Marker>
-                  {postingInfo && (
-                    <DetailPage
-                      displayFlg={displayFlg}
-                      postingInfo={postingInfo}
-                    />
+                          <div className="profile-text py-4 pr-4 relatvie">
+                            <p className="font-semibold text-3xl mb-4 overflow-hidden whitespace-nowrap overflow-ellipsis">
+                              {postingInfo.PetName}
+                            </p>
+                            <p className="h-3/5 overflow-scroll">
+                              {postingInfo.PetInfo}
+                            </p>
+                            <button
+                              className="detailPageBtn w-12 top-1/4 -right-12 bg-yellow-400 absolute h-1/2"
+                              onClick={() => {
+                                setdisplayFlg(true);
+                              }}
+                            >
+                              <Arrow />
+                            </button>
+                          </div>
+                        </div>
+                        <p className="absolute bottom-3 right-3 text-gray-500 ">
+                          {new Date(postingInfo.LostDate).toLocaleDateString()}
+                        </p>
+                      </>
+                    </InfoWindow>
                   )}
-                </>
-              );
-            })}
-          </GoogleMap>
-        </LoadScript>
-      </div>
-    </>
+                </Marker>
+                {postingInfo && (
+                  <DetailPage
+                    displayFlg={displayFlg}
+                    postingInfo={postingInfo}
+                  />
+                )}
+              </>
+            );
+          })}
+        </GoogleMap>
+      </LoadScript>
+    </div>
   );
 }
 
