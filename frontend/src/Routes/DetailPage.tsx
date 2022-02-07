8bbb4aea778b443b1bad2b9e5a44fed133bf4495
han <copetit412@gmail.com>
2022-02-07 21:04:30 +0900

    add swiper #99

--- frontend/src/Routes/DetailPage.tsx
+++ frontend/src/Routes/DetailPage.tsx
@@ -1,23 +1,47 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Autoplay } from 'swiper';
+
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+
+SwiperCore.use([Navigation, Autoplay]);
+
 function DetailPage(props: any) {
   const { displayFlg, postingInfo } = props;
   const petImgs = postingInfo.contents.imageUrl;
-
   return (
     <>
       <div
         className={`${
-          displayFlg ? 'slide-show' : ''
+          displayFlg ? '' : 'slide-show'
         } posting-detail-info h-92/100 w-4/12 -right-1/2 bg-white absolute overflow-scroll `}
       >
-        {petImgs.map((img: String, index: number) => {
-          return (
-            <img
-              src={`${process.env.REACT_APP_API_URL}/${img}`}
-              alt="pet"
-              key={index}
-            />
-          );
-        })}
+        <div className="img-slide">
+          <Swiper
+            className="mySwiper"
+            slidesPerView={1}
+            spaceBetween={30}
+            grabCursor={true}
+            loop={true}
+            navigation={true}
+            autoplay={{
+              delay: 4000,
+              disableOnInteraction: true,
+            }}
+            centeredSlides={true}
+          >
+            {petImgs.map((img: String, index: number) => {
+              return (
+                <SwiperSlide key={index}>
+                  <img
+                    src={`${process.env.REACT_APP_API_URL}/${img}`}
+                    alt="pet"
+                  />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
         <ul>
           <li>
             連絡先:
