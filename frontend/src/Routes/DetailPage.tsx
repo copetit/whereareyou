a14992ed908f099417a0811b9f564cb28ca4f07e
han <copetit412@gmail.com>
2022-02-10 20:12:40 +0900

    スライド表示ページのデザイン反映 #100

--- frontend/src/Routes/DetailPage.tsx
+++ frontend/src/Routes/DetailPage.tsx
@@ -14,7 +14,7 @@ function DetailPage(props: any) {
       <div
         className={`${
           displayFlg ? 'page-slide-show' : ''
-        } posting-detail-info h-92/100 w-4/12 -right-1/2 bg-white absolute overflow-scroll p-5`}
+        } posting-detail-info h-92/100 w-4/12 -right-1/2 bg-white absolute overflow-scroll p-10`}
       >
         <div className="img-slide mb-16">
           {petImgs.length === 1 ? (
@@ -50,36 +50,67 @@ function DetailPage(props: any) {
             </Swiper>
           )}
         </div>
-        <ul>
-          <li>
-            連絡先:
-            {postingInfo.user.MailAddress}
-          </li>
-          <li>
-            名前:
-            {postingInfo.PetName}
-          </li>
-          <li>
-            性別:
-            {postingInfo.PetSex}
-          </li>
-          <li>
-            年齢:
-            {postingInfo.PetAge}
-          </li>
-          <li>
-            離れた日:
-            {new Date(postingInfo.LostDate).toLocaleDateString()}
-          </li>
-          <li>
-            特徴:
-            {postingInfo.PetInfo}
-          </li>
-          <li>
-            その他の情報:
-            {postingInfo.Detail}
-          </li>
-        </ul>
+        <div className="flex justify-center items-center flex-wrap ">
+          <div className="pet-name">
+            <p className="text-4xl">{postingInfo.PetName}</p>
+          </div>
+          <div className="m-5 overflow-x-auto">
+            <div className="inline-block py-2 min-w-full px-8">
+              <div className="overflow-hidden sm:rounded-lg">
+                <table>
+                  <tbody>
+                    <tr className="bg-white border-b">
+                      <td className="py-4 px-6 font-semibold text-gray-900 whitespace-nowrap">
+                        離れた日
+                      </td>
+                      <td className="py-4 px-6 text-gray-600 whitespace-nowrap ">
+                        {new Date(postingInfo.LostDate).toLocaleDateString()}
+                      </td>
+                    </tr>
+                    <tr className="bg-white border-b">
+                      <td className="py-4 px-6 font-semibold text-gray-900 whitespace-nowrap">
+                        性別
+                      </td>
+                      <td className="py-4 px-6 text-gray-600 whitespace-nowrap">
+                        {postingInfo.PetSex}
+                      </td>
+                    </tr>
+                    <tr className="bg-white border-b">
+                      <td className="py-4 px-6 font-semibold text-gray-900 whitespace-nowrap">
+                        年齢
+                      </td>
+                      <td className="py-4 px-6 text-gray-600 whitespace-nowrap">
+                        {postingInfo.PetAge}さい
+                      </td>
+                    </tr>
+                    <tr className="bg-white border-b">
+                      <td className="py-4 px-6 font-semibold text-gray-900 whitespace-nowrap">
+                        連絡先
+                      </td>
+                      <td className="py-4 px-6 text-gray-600">
+                        {postingInfo.user.MailAddress}
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="pet-info-section w-full">
+          <div className="pet-info my-6">
+            <p className="font-semibold px-8 py-2">特徴</p>
+            <p className="text-gray-600 px-8 py-2 w-full break-words">
+              {postingInfo.PetInfo}
+            </p>
+          </div>
+          <div className="pet-detail my-6 ">
+            <p className="font-semibold px-8 py-2">その他の情報</p>
+            <p className="text-gray-600 px-8 py-2 w-full break-words">
+              {postingInfo.Detail}
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
