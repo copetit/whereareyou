970bc9b0cfed0f81af9a827c0f914e8af9c682ff
han <copetit412@gmail.com>
2022-02-17 20:30:14 +0900

    デザイン修正 #107

--- frontend/src/Routes/PasswordChkModal.tsx
+++ frontend/src/Routes/PasswordChkModal.tsx
@@ -1,5 +1,6 @@
-import { ReactComponent as BtnCancel } from '../btn_cancel.svg';
 import { IPasswordChkModal } from '../Types';
+import { ReactComponent as BtnCancel } from '../btn_cancel.svg';
+import { ReactComponent as LockMark } from '../lock_mark.svg';
 
 function PasswordChkModal(props: Required<IPasswordChkModal>) {
   const { isOpen, setShowModal } = props;
@@ -10,10 +11,10 @@ function PasswordChkModal(props: Required<IPasswordChkModal>) {
         <div className="modal-bg w-full h-full bg-black absolute top-0 left-0 bg-opacity-90"></div>
         <div className="modal bg-slate-800 bg-opacity-50 flex justify-center items-center absolute top-0 right-0 bottom-0 left-0">
           <div className="flex flex-col justify-between h-96 relative bg-white w-full max-w-2xl rounded-lg shadow">
-            <div className="flex justify-end p-2">
+            <div className="absolute right-2 top-2">
               <button
                 type="button"
-                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center "
+                className="text-black bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center "
                 data-modal-toggle="authentication-modal"
                 onClick={() => setShowModal(false)}
               >
@@ -21,9 +22,12 @@ function PasswordChkModal(props: Required<IPasswordChkModal>) {
               </button>
             </div>
             <form
-              className="flex flex-col h-92/100 px-10 pt-2 pb-8 space-y-10 xl:pb-8"
+              className="flex flex-col h-full px-10 py-8 space-y-5 xl:pb-8"
               action="#"
             >
+              <div className="flex w-full justify-center h-1/4 mb-2">
+                <LockMark />
+              </div>
               <h3 className="font-medium text-gray-900 text-center">
                 登録時のパスワードを入力してください
               </h3>
